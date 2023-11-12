827efea59b0ba28d65bc4a977d03e93828eaf52f
19kyosuke37 <19kyosuke37@gmail.com>
2023-11-13 00:10:53 +0900

    課題提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,22 +2,22 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        int price = 1700;
+        int price = 1500;
         int tax = getTax(price);
         int noTax = getNoTax(tax,price);
-        
+
         System.out.println(price+"円の商品の税込価格は"+tax+"円（消費税は"+noTax+"円）です。");
     }
-    
+
     public static int getTax(int price) {
         int result = price + (price/10);
         return result;
-        
+
     }
     public static int getNoTax(int tax,int price) {
         int result = tax - price;
         return result;
     }
-    
+
 
 }
